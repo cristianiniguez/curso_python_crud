d288b19d87b1c9bd434b1038bebef0c34316bc8f
cristianiniguez <felizpc1@gmail.com>
2021-07-15 21:01:52 -0400

    basicos del lenguaje: usando funciones en nuestro proyecto

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 clients = 'pablo,ricardo,'
 
-if __name__ == '__main__':
-    clients += 'david'
+
+def create_client(client_name):
+    global clients
+    clients += client_name
+    _add_comma()
+
+
+def read_clients():
+    global clients
     print(clients)
 
+
+def _add_comma():
+    global clients
+    clients += ','
+
+
+if __name__ == '__main__':
+    read_clients()
+    create_client('david')
+    read_clients()
