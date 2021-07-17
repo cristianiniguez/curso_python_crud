de568381a91d917e53737ef590b74d2c45e2e409
cristianiniguez <felizpc1@gmail.com>
2021-07-17 18:08:58 -0400

    uso de strings y ciclos: while loops

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 clients = 'pablo,ricardo,'
 
 
@@ -58,7 +60,19 @@ def _print_welcome():
 
 
 def _get_client_name():
-    return input('What is the client name? ')
+    client_name = None
+
+    while not client_name:
+        client_name = input("What is the client name? ('exit' to exit) ")
+
+        if client_name == 'exit':
+            client_name = None
+            break
+
+    if not client_name:
+        sys.exit()
+
+    return client_name
 
 
 def _print_not_found():
